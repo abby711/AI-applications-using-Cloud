9ba8bbe1f3ffe5df104df3e8cb27b4ccf8cfbbfb
Abirami <65455645+abby711@users.noreply.github.com>
2020-12-29 18:48:22 +0530

    Update color-recognition.py

--- color-recognition.py
+++ color-recognition.py
@@ -6,7 +6,7 @@ url="https://api.imagga.com/v2/colors"
 querystr={"image_url":"https://thumbs.dreamstime.com/z/people-group-selfie-friendly-guy-makes-photo-smiling-friends-smartphone-camera-hands-taking-self-portrait-photos-131283306.jpg "}
 headers={
     'accept':"application/json",
-    'authorization': "Basic YWNjX2QzZmI3ZjU0OTI2OTFmYTphODlhMmM3NGY4ZTllYWJhOTE3NTNiMjVmOGQ4ZDYxMg=="
+    'authorization': "Basic =="
 }
 
 
